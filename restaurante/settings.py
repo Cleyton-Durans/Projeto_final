76d8f355ee2154a2d632faff76913121c63cf28a
Yasmin <cleytondurans.n@gmail.com>
2025-11-05 20:14:01 -0300

    statcfiles

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
             'libraries':{   
+                'staticfiles': 'django.templatetags.static',
                 'static': 'django.templatetags.static',
                 'media': 'django.templatetags.media',
             }
