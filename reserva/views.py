6dd2fcedfed1ce13d421a688fc9aaf5e1b497e1d
Yasmin <cleytondurans.n@gmail.com>
2026-05-03 19:33:18 -0300

    Resolvindo erro de rota de login e logout. Configurado set em pt-br. resolvendo formulário (form) usuário.

--- reserva/views.py
+++ reserva/views.py
@@ -52,7 +52,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect('login') 
 
 # ------------------------
 # Páginas do painel (só staff)
